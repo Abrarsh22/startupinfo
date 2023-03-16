7e8b0ac675d728f2b89c42fe3f2cad8cc4ccf3f0
Abrar Mohammed Shaikh <75785223+Abrarsh22@users.noreply.github.com>
2023-03-16 20:09:33 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000;
 //     console.log(`Hello middleware`);
 //     next();
 // }
+const cors = require("cors");
 
+app.use(cors());
 const db= 'mongodb+srv://Abrarsh2209:yfh5JEt7lTb53ZxM@cluster0.2kgbnxq.mongodb.net/AbrarAPI?retryWrites=true&w=majority';
 mongoose.connect(db).then(()=>{
     console.log("Success");
